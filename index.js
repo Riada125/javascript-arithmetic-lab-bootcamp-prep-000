454054696006e53cdd3a5b86a15a5b8fa0e0302e
Michael Adair <riada125@hotmail.com>
2019-04-23 19:36:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 function add(a,b) {
 return(a + b);
 }
-console.log(add)
+
 function subtract(a,b){
   return(a-b)
 }
-console.log(subtract)
+
 function multiply(a,b){
   return(a*b)
 }
-console.log(multiply)
+
 function divide(a,b){
   return(a/b)
 }
-console.log(divide)
+
 function inc(a){
 }
 function inc(n){
