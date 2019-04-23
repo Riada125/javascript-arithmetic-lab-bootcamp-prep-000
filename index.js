4c69e439add4212a6e59664b2d0de9b3b58e27f7
Michael Adair <riada125@hotmail.com>
2019-04-23 19:31:11 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,41 @@
+function add(a,b) {
+return(a + b);
+}
+console.log(add)
+function subtract(a,b){
+  return(a-b)
+}
+console.log(subtract)
+function multiply(a,b){
+  return(a*b)
+}
+console.log(multiply)
+function divide(a,b){
+  return(a/b)
+}
+console.log(divide)
+function inc(a){
+}
+function inc(n){
+  return(n+=1)
+}
+console.log(inc)
+function dec(n){
+  return(n-=1)
+}
+console.log(dec);
+
+function makeInt(n){
+parseInt('0x2328', 10);
+return(n)
+}
+console.log(makeInt)
+
+function preserveDecimal(n){
+  parseFloat('2.222')
+  return(n)
+}
+console.log(preserveDecimal)
+
+
+
